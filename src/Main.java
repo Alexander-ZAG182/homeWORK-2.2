a0e903eb3a4e5f2e0f594f1d4e3f588b0db6a4c1
Alexander-ZAG182 <hado18190@gmail.com>
2024-11-04 00:12:17 +0300

    start hw2.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+      int a = 40000;
+      byte b = 27;
+      short c = 30000;
+      long d = 1000000000;
+      float e = 3.75F;
+      double f = 3.123456789F;
+        System.out.println( "Значение переменной " + a + " с типом ");
+
+
+
+
+
+
+
+
+
+
+
     }
 }
